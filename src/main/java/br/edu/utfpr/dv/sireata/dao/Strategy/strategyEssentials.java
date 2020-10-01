1f6ad8f82e0756bc9cbbcdd3f9952fa32880b144
unknown <gabrielantalsilva@gmail.com>
2020-10-01 20:46:07 -0300

    Declarado os métodos sememlhantes entre as classes do pacote Dao

--- src/main/java/br/edu/utfpr/dv/sireata/dao/Strategy/strategyEssentials.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/Strategy/strategyEssentials.java
@@ -1,5 +1,23 @@
 package br.edu.utfpr.dv.sireata.dao.Strategy;
 
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+
+
 public abstract class strategyEssentials<Essentials> {
 
+	
+	public abstract int buscaPorID(int id) throws SQLException;
+	
+	public abstract int salvar(Essentials ess) throws SQLException;
+	
+	public abstract List<Essentials> listarTodos(boolean apenasAtivos) throws SQLException;
+	
+	public abstract void excluir(int id) throws SQLException;
+	
+	
+	
 }
