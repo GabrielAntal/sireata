6cb9a648fe4f13b5f0a16e2ae99a02025411c213
unknown <gabrielantalsilva@gmail.com>
2020-10-01 21:44:08 -0300

    Feit3 testes na classe pautaTests referente a classe Pauta do pacote model

--- src/main/java/br/edu/utfpr/dv/sireata/model/pautaTests.java
+++ src/main/java/br/edu/utfpr/dv/sireata/model/pautaTests.java
@@ -2,5 +2,43 @@ package br.edu.utfpr.dv.sireata.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class pautaTests {
+	
+	
+	@Test
+	void compId() {
+		Pauta pa = new Pauta();
+		
+		pa.setIdPauta(17);
+		
+		int num = pa.getIdPauta();
+		
+		assertEquals(17, num);
+	} 
+	
+	
+	@Test
+	void compDesc() {
+		Pauta pa = new Pauta();
+		
+		pa.setDescricao("Ciência sobre terra plana");
+		
+		String num = pa.getDescricao();
+		String desc = "Ciência sobre terra plana";
+		
+		assertEquals(desc, num);
+	}
+	
+	
+	@Test
+	void compTitulo() {
+		Pauta pa = new Pauta();
+		
+		pa.setTitulo("Terra é Plana");
+		
+		String num = pa.getTitulo();
+		String til = "Terra é um Geóide ";
+		
+		assertEquals(til, num);
+	} 
 
 }
